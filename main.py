890c1d732e734866b8e747f4ff5a93210ea23be2
yo-ru <32829880+Yo-ru@users.noreply.github.com>
2020-12-07 14:24:38 -0600

    more testing?

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def init_ball(right: bool) -> None:
     global ball_pos, ball_vel
 
     # Set Ball Position
-    ball_pos = [WIDTH//2, HEIGHT//2]
+    ball_pos = [WIDTH/2, HEIGHT/2]
 
     # Get A Random Horizontal And Vertical Value
     horz = random.randrange(2, 4)
@@ -134,7 +134,7 @@ def draw(window) -> None:
     elif int(ball_pos[0]) <= BALL_RADIUS + PAD_WIDTH:
         right_score += 1
         log(f"Right Scored! ({right_score})", Ansi.LBLUE)
-        #init_ball(True)
+        init_ball(True)
     if int(ball_pos[0]) >= WIDTH + 1 - BALL_RADIUS - PAD_WIDTH and int(ball_pos[1]) in range(pad2_pos[1] - PAD_HEIGHT_HALF, pad2_pos[1] + PAD_HEIGHT_HALF, 1):
         ball_vel[0] = -ball_vel[0]
         ball_vel[0] *= 1.1
@@ -142,7 +142,7 @@ def draw(window) -> None:
     elif int(ball_pos[0]) >= WIDTH + 1 - BALL_RADIUS - PAD_WIDTH:
         left_score += 1
         log(f"Left Scored! ({left_score})", Ansi.LBLUE)
-        #init_ball(False)
+        init_ball(False)
 
     # Update and Draw Scores
     font = pygame.font.SysFont("Comic Sans MS", 20)
